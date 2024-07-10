d07a5687c223e6539f44ba31816ebbf64dfd223b
MultiTron <iliev_evn@hotmail.com>
2024-07-11 02:05:54 +0300

    Added: Chuck Norris quote API request

---
+++ assets/js/quote.js
@@ -0,0 +1,10 @@
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = () => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        let json = JSON.parse(xhttp.responseText);
+        document.getElementById('quote').textContent = json.value;
+    }
+}
+xhttp.open('GET', 'https://api.chucknorris.io/jokes/random', true);
+xhttp.send();
